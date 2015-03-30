b0f0a18fd7e536292f0b3ebcd78f0a7fe4334707
Sam <sam.a.menza@gmail.com>
2015-03-30 08:46:09 -0400

    Added text and button views

--- app/src/main/java/sam/hello_world/MainActivity.java
+++ app/src/main/java/sam/hello_world/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mainTextView;
+        mainTextView=(TextView)findViewById(R.id.main_textview);
+        mainTextView.setText("I set the id in Java!");
     }
 
 
